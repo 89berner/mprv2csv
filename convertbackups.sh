8c0794a360add007ced2e6999d2b69823823c41c
89berner <89berner@gmail.com>
2015-05-17 20:19:40 -0300

    Update convertbackups.sh

--- convertbackups.sh
+++ convertbackups.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-POL=$1
-DATE=$2
-
-if [  -n "${POL}" ]; then
-  if [  -n "${DATE}" ]; then
-    echo "Looking for all files"
-    for f in `ls impervafiles/SecureSphere_Audit_@${POL}_$DATE*`; do
-      echo "Proceso a -> $f"
-      ./processbackup.sh $f
-    done
-    echo "Done!"
-  else
-    echo "You must specify a POLICY"
-  fi
-else
-  echo "You must specify a DATE"
-fi
+for f in `ls impervafiles`; do
+  echo "Proceso a -> $f"
+  ./processbackup.sh impervafiles/$f
+done
+echo "Done!"
