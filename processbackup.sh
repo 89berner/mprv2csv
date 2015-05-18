03992d6e1972e999a324df3b0e14d9ae1b29fbba
89berner <89berner@gmail.com>
2015-05-17 21:59:36 -0300

    Update processbackup.sh

--- processbackup.sh
+++ processbackup.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 
-FILENAME=$1
-
-if [ ! -d temp ]; then
-  mkdir temp
-fi
-
-if [ ! -d processed ]; then
-  mkdir processed
-fi
-
-if [  -n "${FILENAME}" ]; then
-  if [ -f $FILENAME ]; then
-
-      NAME=$(basename $1)
-    
-      echo "processbackup.sh: Trying to rpocess $FILENAME"
+for f in `ls impervafiles`; do
+  echo "processbackup.sh: Proceso a -> $f"
+  FILENAME=$f
+  
+  if [ ! -d temp ]; then
+    mkdir temp
+  fi
+  
+  if [ ! -d processed ]; then
+    mkdir processed
+  fi
+  
+  if [  -n "${FILENAME}" ]; then
+    if [ -f $FILENAME ]; then
+  
+        NAME=$(basename $f)
       
-      if [ ! -d $NAME-dir ]; then
-        echo "processbackup.sh: Creating folder $NAME-dir"
-        mkdir $NAME-dir
+        echo "processbackup.sh: Trying to rpocess $FILENAME"
+        
+        if [ ! -d $NAME-dir ]; then
+          echo "processbackup.sh: Creating folder $NAME-dir"
+          mkdir $NAME-dir
+        else
+          echo "processbackup.sh: Deleting folder $NAME-dir"
+          rm -rf $NAME-dir/*
+        fi
+        
+        echo "processbackup.sh: Copying to ./tmp file $NAME"
+        cp $FILENAME ./temp/
+        
+        echo "processbackup.sh: Executing packagertool.jar"
+        java -jar packagertool.jar -unpack -source temp/$NAME -target $NAME-dir/
+        
+        echo "processbackup.sh: Now running convertAuditFiles.sh"
+        
+        ./convertAuditFiles.sh $NAME-dir
+        
+        echo "processbackup.sh: Now running joinfiles.rb"
+        
+        ruby joinfiles.rb $NAME-dir/
+        
+        rm temp/$NAME* 2>/dev/null
       else
-        echo "processbackup.sh: Deleting folder $NAME-dir"
-        rm -rf $NAME-dir/*
+        echo "processbackup.sh: The file $FILENAME does not exist!"
       fi
-      
-      echo "processbackup.sh: Copying to ./tmp file $NAME"
-      cp $FILENAME ./temp/
-      
-      echo "processbackup.sh: Executing packagertool.jar"
-      java -jar packagertool.jar -unpack -source temp/$NAME -target $NAME-dir/
-      
-      echo "processbackup.sh: Now running convertAuditFiles.sh"
-      
-      ./convertAuditFiles.sh $NAME-dir
-      
-      echo "processbackup.sh: Now running joinfiles.rb"
-      
-      ruby joinfiles.rb $NAME-dir/
-      
-      rm temp/$NAME* 2>/dev/null
-    else
-      echo "processbackup.sh: The file $FILENAME does not exist!"
-    fi
-else
-  echo "You must send a file as an argument"
-fi
+  else
+    echo "You must send a file as an argument"
+  fi
+done
+echo "processbackup.sh: Done!"
